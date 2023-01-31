5dcf6aca5e02f5c0747e32047c20d26a1d152fc3
AnarMikailov <a.v.mikayilov@gmail.com>
2023-01-31 16:14:51 +0400

    finised

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import MovieList from './components/MovieList';
-import Search from './components/Search';
-import MovieListHeading from './components/MovieListHeading';
-import AddFavourities from './components/AddFavourities';
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./components/MovieList";
+import Search from "./components/Search";
+import MovieListHeading from "./components/MovieListHeading";
+import AddFavourities from "./components/AddFavourities";
+import RemoveFavourities from "./components/RemoveFavourities";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [search, setSearch] = useState('');
+  const [favourities, setFavourities] = useState([]);
+  const [search, setSearch] = useState("");
 
-  const inputChangeHandler = e => {
+  const inputChangeHandler = (e) => {
     e.preventDefault();
-    if (e.target.value) {
-      setSearch(e.target.value);
-    }
+
+    setSearch(e.target.value);
   };
-  const fetchApi = async search => {
+  const fetchApi = async (search) => {
     const url = ` http://www.omdbapi.com/?s=${search}&apikey=bbf8f75e`;
     const response = await fetch(url);
     const responseJson = await response.json();
@@ -27,6 +28,27 @@ function App() {
   useEffect(() => {
     fetchApi(search);
   }, [search]);
+  const saveLocalStorage = (items) => {
+    localStorage.setItem("movies", JSON.stringify(items));
+  };
+  useEffect(() => {
+    const movieFavourities = JSON.parse(localStorage.getItem("movies"));
+    setFavourities(movieFavourities);
+  }, []);
+  const addFavouritiesHandler = (movie) => {
+    const newFavourities = [...favourities, movie];
+
+    if (!favourities.includes(movie)) {
+      setFavourities(newFavourities);
+    }
+    saveLocalStorage(movies);
+  };
+  const removeFavouritiesHandler = (movie) => {
+    const filetered = favourities.filter((el) => el != movie);
+    setFavourities(filetered);
+    saveLocalStorage(filetered);
+  };
+
   return (
     <div className="container-fluid  movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -35,7 +57,22 @@ function App() {
       </div>
 
       <div className="row">
-        <MovieList favouritecomponent={AddFavourities} movies={movies} />
+        <MovieList
+          handleFavourities={addFavouritiesHandler}
+          favouritecomponent={AddFavourities}
+          movies={movies}
+        />
+      </div>
+
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourities" />
+      </div>
+      <div className="row">
+        <MovieList
+          handleFavourities={removeFavouritiesHandler}
+          favouritecomponent={RemoveFavourities}
+          movies={favourities}
+        />
       </div>
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function MovieList(props) {
   const FavouriteComponent = props.favouritecomponent;
-  const [addFavourities, setAddFavourities] = useState(null);
   return (
     <>
-      {props.movies.map(movie => (
-        <div className="image-container d-flex justify-content-start m-3">
+      {props.movies.map((movie) => (
+        <div
+          key={movie.imdbID}
+          className="image-container d-flex justify-content-start m-3"
+        >
           <img src={movie.Poster} alt="movies" />
-          <div className="overlay d-flex justify-content-center align-items-center">
+          <div
+            onClick={() => {
+              props.handleFavourities(movie);
+            }}
+            className="overlay d-flex justify-content-center align-items-center"
+          >
             <FavouriteComponent />
           </div>
         </div>
